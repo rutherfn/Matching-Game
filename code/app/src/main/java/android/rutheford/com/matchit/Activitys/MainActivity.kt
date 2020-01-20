331002d2213078cbebde47ea7ad4c2477a582b8b
Nicholas Rutherford <rutherfn@gmatc.matc.edu>
2020-01-20 00:01:57 -0600

    cleaned up code, done with matching game

--- code/app/src/main/java/android/rutheford/com/matchit/Activitys/MainActivity.kt
+++ code/app/src/main/java/android/rutheford/com/matchit/Activitys/MainActivity.kt
@@ -4,11 +4,14 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.rutheford.com.matchit.R
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.home_main_view.*
 
 class MainActivity : AppCompatActivity() {
+
     // declarations
     private val arrayListForImages: MutableList<Int> = mutableListOf(R.drawable.tigers,R.drawable.bears,R.drawable.giraffes,R.drawable.tigers,R.drawable.bears,R.drawable.giraffes,
         R.drawable.tigers,R.drawable.bears,R.drawable.giraffes,R.drawable.tigers,R.drawable.bears,R.drawable.giraffes)
@@ -22,24 +25,59 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_main_view)
-        Main();
+        funcForMain();
     }
-    private fun Main() {
+    private fun funcForMain() {
         arrayListForImages.shuffle()
+        funcForMatchingImages()
+        funcForReset()
+        // set up images
+    }
+    private fun alertForNonMatch(i: Int, images: Array<ImageView>, arrayListForStock: MutableList<Int>, firstClicked: Int,lastClicked: Int){
+        val builder = AlertDialog.Builder(this@MainActivity)
 
-        val matchingImageViewOne = findViewById<ImageView>(R.id.matchingImageViewOne)
-        val matchingImageViewTwo = findViewById<ImageView>(R.id.matchingImageViewTwo)
-        val matchingImageViewThree = findViewById<ImageView>(R.id.matchingImageViewThree)
-        val matchingImageViewFour = findViewById<ImageView>(R.id.matchingImageViewFour)
-        val matchingImageViewFive = findViewById<ImageView>(R.id.matchingImageViewFive)
-        val matchingImageViewSix = findViewById<ImageView>(R.id.matchingImageViewSix)
-        val matchingImageViewSeven = findViewById<ImageView>(R.id.matchingImageViewSeven)
-        val matchingImageViewEight = findViewById<ImageView>(R.id.matchingImageViewEight)
-        val matchingImageViewNine = findViewById<ImageView>(R.id.matchingImageViewNine)
-        val matchingImageViewTen = findViewById<ImageView>(R.id.matchingImageViewTen)
-        val matchingImageViewEleven = findViewById<ImageView>(R.id.matchingImageViewEleven)
-        val matchingImageViewTwelve = findViewById<ImageView>(R.id.matchingImageViewTwelve)
+        // Set the alert dialog title
+        builder.setTitle("Did Not Find A Match")
 
+        // Display a message on alert dialog
+        builder.setMessage("Sorry no match, press OK to continue ")
+
+        // Set a positive button and its click listener on alert dialog
+        builder.setPositiveButton("OK"){dialog, which ->
+            images[firstClicked].setImageResource(arrayListForStock.get(i))
+            images[lastClicked].setImageResource(arrayListForStock.get(i))
+        }
+
+        // Finally, make the alert dialog using builder
+        val dialog: AlertDialog = builder.create()
+
+        // Display the alert dialog on app interface
+        dialog.show()
+    }
+    private fun alertForWinner(){
+        val builder = AlertDialog.Builder(this@MainActivity)
+
+        // Set the alert dialog title
+        builder.setTitle("Congrats You Won")
+
+        // Display a message on alert dialog
+        builder.setMessage("You found all the matches! Press OK to continue! ")
+
+        // Set a positive button and its click listener on alert dialog
+        builder.setPositiveButton("OK"){dialog, which ->
+            resetGame()
+        }
+
+        // Finally, make the alert dialog using builder
+        val dialog: AlertDialog = builder.create()
+
+        // Display the alert dialog on app interface
+        dialog.show()
+    }
+    private fun resetImageToUnknown(imageView: ImageView){
+        imageView.setImageResource(R.drawable.question)
+    }
+    private fun funcForMatchingImages(){ // setting up array list for images
         val images = arrayOf(
             matchingImageViewOne,
             matchingImageViewTwo,
@@ -54,10 +92,7 @@ class MainActivity : AppCompatActivity() {
             matchingImageViewEleven,
             matchingImageViewTwelve
         )
-        arrayListForImages.forEach {
-            println(it)
-        }
-        for (i in 0..11) {
+        for (i in 0..11) { // logic for matching items.
             images[i].setImageResource(arrayListForStock.get(i))
             println(images[i].tag)
             images[i].setOnClickListener {
@@ -69,43 +104,35 @@ class MainActivity : AppCompatActivity() {
                     lastClicked = i;
                     images[i].setImageResource(arrayListForImages.get(i))
                     if(arrayListForImages[firstClicked] == arrayListForImages[lastClicked]){
-                        println("Match")
                         clicked = 0;
                         // alert that says its matched
                     }else{
                         println("No match")
                         clicked = 0;
-                        alertForNonMatch(i,images,images[firstClicked],images[lastClicked],arrayListForStock,firstClicked,lastClicked)
+                        alertForNonMatch(i,images,arrayListForStock,firstClicked,lastClicked)
                     }
                 }
             }
-
-            // main setup for method will go here.
         }
     }
-    private fun alertForNonMatch(i: Int, images: Array<ImageView>, firstClickedImageView: ImageView,secondClickedImageView:ImageView, arrayListForStock: MutableList<Int>, firstClicked: Int,lastClicked: Int){
-        val builder = AlertDialog.Builder(this@MainActivity)
-
-        // Set the alert dialog title
-        builder.setTitle("App background color")
-
-        // Display a message on alert dialog
-        builder.setMessage("Are you want to set the app background color to RED?")
-
-        // Set a positive button and its click listener on alert dialog
-        builder.setPositiveButton("OK"){dialog, which ->
-            // Do something when user press the positive button
-            Toast.makeText(applicationContext,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
-            images[firstClicked].setImageResource(arrayListForStock.get(i))
-            images[lastClicked].setImageResource(arrayListForStock.get(i))
-            // Change the app background color
-            //root_layout.setBackgroundColor(Color.RED)
+    private fun resetGame(){ // reset game func
+        arrayListForImages.shuffle()
+        resetImageToUnknown(matchingImageViewOne)
+        resetImageToUnknown(matchingImageViewTwo)
+        resetImageToUnknown(matchingImageViewThree)
+        resetImageToUnknown(matchingImageViewFour)
+        resetImageToUnknown(matchingImageViewFive)
+        resetImageToUnknown(matchingImageViewSix)
+        resetImageToUnknown(matchingImageViewSeven)
+        resetImageToUnknown(matchingImageViewEight)
+        resetImageToUnknown(matchingImageViewNine)
+        resetImageToUnknown(matchingImageViewTen)
+        resetImageToUnknown(matchingImageViewEleven)
+        resetImageToUnknown(matchingImageViewTwelve)
+    }
+    private fun funcForReset(){
+        buttonReset.setOnClickListener(){
+            resetGame()
         }
-
-        // Finally, make the alert dialog using builder
-        val dialog: AlertDialog = builder.create()
-
-        // Display the alert dialog on app interface
-        dialog.show()
     }
 }
